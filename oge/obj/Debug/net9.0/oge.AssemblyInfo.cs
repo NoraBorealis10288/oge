fdb4714a722484a9b5e1d6414eed0cb3e4986d11
Nora Reese <norar10288@gmail.com>
2025-03-26 14:46:57 -0500

    added icons to navbar

--- oge/obj/Debug/net9.0/oge.AssemblyInfo.cs
+++ oge/obj/Debug/net9.0/oge.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("oge")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+b6364470101adf1d97033aa1804a886f86f44964")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+6f63b24737d43d16577b7f6f9d3e6a68707fa209")]
 [assembly: System.Reflection.AssemblyProductAttribute("oge")]
 [assembly: System.Reflection.AssemblyTitleAttribute("oge")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
