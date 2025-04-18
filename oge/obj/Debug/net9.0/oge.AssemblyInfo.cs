401fc02611c9d3685e86973a4917ea51ad2f5e28
Nora Reese <norar10288@gmail.com>
2025-04-18 13:20:09 -0500

    fixed info page 3 to correctly display averages instead of total

--- oge/obj/Debug/net9.0/oge.AssemblyInfo.cs
+++ oge/obj/Debug/net9.0/oge.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("oge")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+0a021b1581ea078b439252857bb5b7914a57fee2")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+2734623dcf5e4c5ea55bf1505023d58560a3f39d")]
 [assembly: System.Reflection.AssemblyProductAttribute("oge")]
 [assembly: System.Reflection.AssemblyTitleAttribute("oge")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
